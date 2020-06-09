a6f932750a563647df1ff12255931b6f0c71629d
François Pluchino <francois.pluchino@klipper.dev>
2020-06-09 18:09:30 +0200

    Add simple spacer component

---
+++ components/KSimpleSpacer/KSimpleSpacer.vue
@@ -0,0 +1,12 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<template>
+    <v-spacer></v-spacer>
+</template>
