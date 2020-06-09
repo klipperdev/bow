bc96f82a59387923939183ad8fff02ccad5f826b
François Pluchino <francois.pluchino@klipper.dev>
2020-06-09 16:42:18 +0200

    Add mixin for the slot wrapper

---
+++ mixins/SlotWrapper.ts
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import {SlotWrapperItem} from '../slots/SlotWrapperItem';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+@Component
+export class SlotWrapper extends Vue {
+    public getSlotItems(prefix: string): SlotWrapperItem[] {
+        const items = [];
+
+        for (const slotName of Object.keys(this.$scopedSlots)) {
+            if (prefix === slotName || slotName.startsWith(prefix + '.')) {
+                const subName = slotName.substring(prefix.length + 1);
+                items.push({
+                    original: slotName,
+                    target: subName ? subName : 'default',
+                } as SlotWrapperItem);
+            }
+        }
+
+        return items;
+    }
+}
