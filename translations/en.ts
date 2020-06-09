190a49e56edb217777efb40292f2b4cbb91811fc
François Pluchino <francois.pluchino@klipper.dev>
2020-06-09 12:03:18 +0200

    Add translations

---
+++ translations/en.ts
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export default {
+    'sw.app.updated': 'A new version of the application is available. Restart the application to use it',
+    'next.count': 'Next ({count})',
+    'close': 'Close',
+    'cancel': 'Cancel',
+    'delete': 'Delete',
+    'delete.confirmation.text': 'Are you sure you want to delete this record?',
+    'refresh': 'Refresh',
+    'show': 'Show',
+    'add': 'Add',
+    'edit': 'Edit',
+    'save': 'Save',
+    'logout': 'Logout',
+    'search': 'Search',
+    'retry': 'Retry',
+    'stop': 'Stop',
+    'source': 'Source',
+    'online': 'Online',
+    'offline': 'Offline',
+    'error.network': 'No network connection detected',
+    'error.internal': 'An error has occurred...',
+    'error.404-page-not-found': 'Oops! Page not found',
+    'error.go-to-home': 'Go to home',
+    'This value is required': 'This value is required',
+};
---
+++ translations/fr.ts
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export default {
+    'sw.app.updated': 'Une nouvelle version de l\'application est disponible. Relancez l\'application pour l\'utiliser',
+    'next.count': 'Suivant ({count})',
+    'close': 'Fermer',
+    'cancel': 'Annuler',
+    'delete': 'Supprimer',
+    'delete.confirmation.text': 'Voulez-vous vraiment supprimer cette enregistrement ?',
+    'refresh': 'Actualiser',
+    'show': 'Afficher',
+    'add': 'Ajouter',
+    'edit': 'Modifier',
+    'save': 'Enregistrer',
+    'logout': 'Déconnexion',
+    'search': 'Rechercher',
+    'retry': 'Recommencer',
+    'stop': 'Arrêter',
+    'source': 'Source',
+    'online': 'En ligne',
+    'offline': 'Hor ligne',
+    'error.network': 'Aucune connexion réseau détectée',
+    'error.internal': 'Une erreur s\'est produite...',
+    'error.404-page-not-found': 'Oops ! Page non trouvée',
+    'error.go-to-home': 'Retourner à la maison',
+    'This value is required': 'Cette valeur est requise',
+};
