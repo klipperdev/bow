db03d27d2cc0180ab4df38fa10a068a486a21a66
François Pluchino <francois.pluchino@klipper.dev>
2020-06-14 11:32:59 +0200

    Add api auth interceptor

--- api/apiInterceptors.ts
+++ api/apiInterceptors.ts
@@ -11,6 +11,7 @@ import {AxiosRequestConfig} from 'axios';
 import {Store} from 'vuex';
 import {KlipperClient} from '@klipper/sdk/KlipperClient';
 import {I18nModuleState} from '../stores/i18n/I18nModuleState';
+import {AuthModuleState} from '../stores/auth/AuthModuleState';
 
 /**
  * Add the locale interceptor.
@@ -20,6 +21,21 @@ import {I18nModuleState} from '../stores/i18n/I18nModuleState';
 export function addLocaleInterceptor(apiClient: KlipperClient, store: Store<I18nModuleState>): void {
     apiClient.addRequestInterceptor((config: AxiosRequestConfig): AxiosRequestConfig => {
         config.headers['Accept-Language'] = store.state.i18n.locale;
+
+        return config;
+    });
+}
+
+/**
+ * Add the auth interceptor.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export function addAuthInterceptor(apiClient: KlipperClient, store: Store<AuthModuleState>): void {
+    apiClient.addRequestInterceptor((config: AxiosRequestConfig): AxiosRequestConfig => {
+        if (undefined === config.auth && !config.headers.Authorization && store.state.auth.accessToken) {
+            config.headers.Authorization = `${store.state.auth.tokenType} ${store.state.auth.accessToken}`;
+        }
 
         return config;
     });
