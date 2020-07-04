c9b3a5a78a813963ede8189876d29cf4a0899a92
François Pluchino <francois.pluchino@klipper.dev>
2020-07-04 09:54:15 +0200

    Add col spacer component

---
+++ components/KColSpacer/KColSpacer.vue
@@ -0,0 +1,24 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<template>
+    <v-col cols="12" sm="6">
+    </v-col>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    /**
+     * @author François Pluchino <francois.pluchino@klipper.dev>
+     */
+    @Component
+    export default class KColSpacer extends Vue {
+    }
+</script>
