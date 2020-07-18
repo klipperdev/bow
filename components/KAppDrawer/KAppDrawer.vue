7c532f77324d935a895e9db0cdfd78eb6c7cd9f3
François Pluchino <francois.pluchino@klipper.dev>
2020-07-18 17:45:20 +0200

    Add transition when drawer items are updated

--- components/KAppDrawer/KAppDrawer.vue
+++ components/KAppDrawer/KAppDrawer.vue
@@ -25,77 +25,79 @@ file that was distributed with this source code.
             </slot>
         </template>
 
-        <v-list rounded>
-            <template v-for="(item, i) in items">
-                <v-tooltip v-if="item.heading"
-                           :key="'item_' + i"
-                           right
-                           open-delay="120"
-                           :disabled="!mini"
-                           nudge-right="8"
-                           eager
-                           transition="slide-x-transition"
-                >
-                    <template v-slot:activator="{on, attrs}">
-                        <v-list-item v-on="on">
-                            <v-list-item-content>
-                                <v-list-item-subtitle>
-                                    {{ item.translatable ? $t(item.heading) : item.heading }}
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </template>
-                    <span>{{ item.translatable ? $t(item.heading) : item.heading }}</span>
-                </v-tooltip>
+        <v-slide-x-transition mode="out-in">
+            <v-list rounded :key="itemKey">
+                <template v-for="(item, i) in items">
+                    <v-tooltip v-if="item.heading"
+                               :key="'item_' + i"
+                               right
+                               open-delay="120"
+                               :disabled="!mini"
+                               nudge-right="8"
+                               eager
+                               transition="slide-x-transition"
+                    >
+                        <template v-slot:activator="{on, attrs}">
+                            <v-list-item v-on="on">
+                                <v-list-item-content>
+                                    <v-list-item-subtitle>
+                                        {{ item.translatable ? $t(item.heading) : item.heading }}
+                                    </v-list-item-subtitle>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </template>
+                        <span>{{ item.translatable ? $t(item.heading) : item.heading }}</span>
+                    </v-tooltip>
 
-                <v-divider
-                        v-else-if="item.divider"
-                        :key="i"
-                        dark
-                        class="my-3"
-                ></v-divider>
+                    <v-divider
+                            v-else-if="item.divider"
+                            :key="i"
+                            dark
+                            class="my-3"
+                    ></v-divider>
 
-                <v-tooltip v-else
-                           right
-                           open-delay="120"
-                           :disabled="!mini"
-                           nudge-right="8"
-                           eager
-                           transition="slide-x-transition"
-                           :color="$store.state.darkMode.enabled ? 'primary lighten-2' : 'primary lighten-1'"
-                           :key="i"
-                >
-                    <template v-slot:activator="{on, attrs}">
-                        <v-list-item
-                                v-on="on"
-                                active-class="primary white--text white--icon"
-                                :ripple="false"
-                                :key="i"
-                                :to="item.route"
-                                :dense="item.dense"
-                                @click.stop="eventClick(item.click)"
-                        >
-                            <v-list-item-icon>
-                                <v-icon :color="$color(item.color)"
-                                        :dense="item.dense"
-                                >{{ item.icon }}</v-icon>
-                            </v-list-item-icon>
-                            <v-list-item-content>
-                                <v-list-item-title :class="item.textClass">
-                                    {{ item.translatable ? $t(item.text) : item.text }}
-                                </v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </template>
-                    <span>{{ item.translatable ? $t(item.text) : item.text }}</span>
-                </v-tooltip>
-            </template>
+                    <v-tooltip v-else
+                               right
+                               open-delay="120"
+                               :disabled="!mini"
+                               nudge-right="8"
+                               eager
+                               transition="slide-x-transition"
+                               :color="$store.state.darkMode.enabled ? 'primary lighten-2' : 'primary lighten-1'"
+                               :key="i"
+                    >
+                        <template v-slot:activator="{on, attrs}">
+                            <v-list-item
+                                    v-on="on"
+                                    active-class="primary white--text white--icon"
+                                    :ripple="false"
+                                    :key="i"
+                                    :to="item.route"
+                                    :dense="item.dense"
+                                    @click.stop="eventClick(item.click)"
+                            >
+                                <v-list-item-icon>
+                                    <v-icon :color="$color(item.color)"
+                                            :dense="item.dense"
+                                    >{{ item.icon }}</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-content>
+                                    <v-list-item-title :class="item.textClass">
+                                        {{ item.translatable ? $t(item.text) : item.text }}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </template>
+                        <span>{{ item.translatable ? $t(item.text) : item.text }}</span>
+                    </v-tooltip>
+                </template>
 
-            <template v-for="(slotItem) in getSlotItems('list')"
-                      v-slot:[slotItem.target]>
-                <slot :name="slotItem.original"></slot>
-            </template>
-        </v-list>
+                <template v-for="(slotItem) in getSlotItems('list')"
+                          v-slot:[slotItem.target]>
+                    <slot :name="slotItem.original"></slot>
+                </template>
+            </v-list>
+        </v-slide-x-transition>
 
         <template v-for="(slotItem) in getSlotItems('drawer')"
                   v-slot:[slotItem.target]>
@@ -120,6 +122,10 @@ file that was distributed with this source code.
     export default class KAppDrawer extends mixins(SlotWrapper) {
         @Prop({type: Array})
         public items!: DrawerItem[]|HeadingDrawerItem[]|DividerDrawerItem[]|TextDrawerItem[];
+
+        public get itemKey(): string {
+            return JSON.stringify(this.items);
+        }
 
         public get mini(): boolean {
             return this.$store && this.$store.state.drawer ? this.$store.state.drawer.mini : false;
