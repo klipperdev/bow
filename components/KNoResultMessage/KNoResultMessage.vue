aa07c0a69e673b84df0ce7866f47f128d3bb3136
François Pluchino <francois.pluchino@klipper.dev>
2020-07-19 11:08:28 +0200

    Add no result message component

---
+++ components/KNoResultMessage/KNoResultMessage.vue
@@ -0,0 +1,45 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<template>
+    <v-row align="center" justify="center">
+        <k-wall-message :message="message || $t('no-result')">
+            <template v-slot:icon>
+                <k-lottie
+                        center
+                        width="90%"
+                        max-width="500px"
+                        :options="{animationData: iconData, loop: false}"
+                ></k-lottie>
+            </template>
+
+            <template v-slot:default>
+                <slot></slot>
+            </template>
+        </k-wall-message>
+    </v-row>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import iconData from '../../assets/animations/searchNoResult.json';
+
+    /**
+     * @author François Pluchino <francois.pluchino@klipper.dev>
+     */
+    @Component
+    export default class KNoResultMessage extends Vue {
+        @Prop({type: String, required: false})
+        public message!: string;
+
+        public get iconData(): object {
+            return  iconData;
+        }
+    }
+</script>
