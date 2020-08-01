1bc5e28020bae5bf3778f0bf386e2ffb80f6dfc0
François Pluchino <francois.pluchino@klipper.dev>
2020-08-01 21:43:35 +0200

    Add a util to check if 2 objects are deep equal

--- utils/object.ts
+++ utils/object.ts
@@ -15,6 +15,35 @@ export function isObject(item: any|null|undefined): boolean {
 }
 
 /**
+ * Check if the object values are equals.
+ */
+export function deepEqual(object1?: object, object2?: object): boolean {
+    if (undefined === object1 || undefined === object2) {
+        return object1 === object2;
+    }
+
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (const key of keys1) {
+        const val1 = (object1 as any)[key];
+        const val2 = (object2 as any)[key];
+        const areObjects = isObject(val1) && isObject(val2);
+
+        if (areObjects && !deepEqual(val1, val2)
+                || !areObjects && val1 !== val2) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
  * Deep merge objects.
  *
  * @param target  The object target
