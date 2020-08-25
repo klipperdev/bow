d673c5c2a9a8a95147d84924c0b3c3cec0ee3ee1
François Pluchino <francois.pluchino@klipper.dev>
2020-08-25 15:51:32 +0200

    Add choice chip component

---
+++ components/KChoiceChip/KChoiceChip.vue
@@ -0,0 +1,33 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<template>
+    <v-chip :color="$oc(choice).color(defaultColor)" dark>
+        {{ $oc(choice).label(defaultLabel) }}
+    </v-chip>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    /**
+     * @author François Pluchino <francois.pluchino@klipper.dev>
+     */
+    @Component
+    export default class KChoiceChip extends Vue {
+        @Prop({type: String, default: 'grey lighten-1'})
+        public defaultColor!: string;
+
+        @Prop({type: String, default: '~'})
+        public defaultLabel!: string;
+
+        @Prop({type: Object|undefined, default: undefined, required: true})
+        public choice!: object|undefined;
+    }
+</script>
