2c6b4acb6b7c69ff12f2e4b3d7a34d289d74a0a1
François Pluchino <francois.pluchino@klipper.dev>
2020-10-31 17:18:32 +0100

    Add countries method on country formatter

--- i18n/CountryFormatter.ts
+++ i18n/CountryFormatter.ts
@@ -10,6 +10,8 @@
 import VueI18n from 'vue-i18n';
 import isoCountries, {LocaleData} from 'i18n-iso-countries';
 import enCountries from 'i18n-iso-countries/langs/en.json';
+import {Country} from '@klipper/bow/i18n/Country';
+import {MapKey} from '@klipper/http-client/models/MapKey';
 
 /**
  * Country formatter.
@@ -18,6 +20,8 @@ import enCountries from 'i18n-iso-countries/langs/en.json';
  */
 export class CountryFormatter {
     private readonly i18n?: VueI18n;
+
+    private cache: MapKey<Country[]> = {} as MapKey<Country[]>;
 
     public constructor(countryLocales: LocaleData[] = [], i18n?: VueI18n) {
         this.i18n = i18n;
@@ -37,5 +41,38 @@ export class CountryFormatter {
         }
 
         return undefined;
+    }
+
+    public countries(): Country[] {
+        const locale = this.i18n ? this.i18n.locale : 'en';
+
+        if (this.cache[locale]) {
+            return this.cache[locale];
+        }
+
+        const countries = isoCountries.getNames(locale);
+        const values = [] as Country[];
+        this.cache = {};
+
+        Object.keys(countries).forEach((code: string) => {
+            values.push({
+                code,
+                name: countries[code],
+            } as Country);
+        });
+
+        values.sort((a: Country, b: Country) => {
+            if (a.name < b.name) {
+                return -1;
+            } else if (a.name > b.name) {
+                return 1;
+            }
+
+            return 0;
+        });
+
+        this.cache[locale] = values;
+
+        return values;
     }
 }
