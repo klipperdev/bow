83fe8a65a3c1ed1b614e9b051f237305ee3dd7b3
François Pluchino <francois.pluchino@klipper.dev>
2020-11-11 00:40:55 +0100

    Add text component

---
+++ components/KText/KText.ts
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {Colorable} from '@klipper/bow/mixins/Colorable';
+import {Themeable} from '@klipper/bow/mixins/Themeable';
+import {mixins} from 'vue-class-component';
+import {Component, Prop} from 'vue-property-decorator';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+@Component
+export default class KText extends mixins(
+    Colorable,
+    Themeable,
+) {
+    @Prop({type: String})
+    public value!: string;
+
+    private get classes(): object {
+        return {
+            'k-text': true,
+            ...this.themeClasses,
+            ...this.textColorClasses,
+        };
+    }
+
+    private get styles(): object {
+        return {
+            ...this.textColorStyles,
+        };
+    }
+}
---
+++ components/KText/KText.vue
@@ -0,0 +1,22 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KText.ts" />
+
+<template>
+    <span
+        v-bind="$attrs"
+        v-on="$listeners"
+        :class="classes"
+        :style="styles"
+        v-html="value"
+    >
+        <slot name="default"/>
+    </span>
+</template>
