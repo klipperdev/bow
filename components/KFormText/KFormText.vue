ec2130d7a3571542165f01c67dd049e9fd7ff408
François Pluchino <francois.pluchino@klipper.dev>
2020-11-11 00:59:59 +0100

    Add form text component

---
+++ components/KFormText/KFormText.vue
@@ -0,0 +1,29 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KFormText.ts" />
+
+<template>
+    <v-text-field
+        v-bind="$attrs"
+        v-on="$listeners"
+        type="text"
+        dense
+        filled
+    >
+        <template v-slot:append><slot name="append"/></template>
+        <template v-slot:append-outer><slot name="append-outer"/></template>
+        <template v-slot:default><slot name="default"/></template>
+        <template v-slot:label><slot name="label"/></template>
+        <template v-slot:message="scope"><slot name="message" v-bind="scope"/></template>
+        <template v-slot:prepend><slot name="prepend"/></template>
+        <template v-slot:prepend-inner><slot name="prepend-inner"/></template>
+        <template v-slot:progress><slot name="progress"/></template>
+    </v-text-field>
+</template>
