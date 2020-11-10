b547803bb20c3c639635472272b8479db8f1f635
François Pluchino <francois.pluchino@klipper.dev>
2020-11-10 16:22:12 +0100

    Add isNumber helper

--- utils/number.ts
+++ utils/number.ts
@@ -15,3 +15,16 @@
 export function randomNumberBetween(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+/**
+ * Check if a value is a number.
+ *
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export function isNumber(value: any): boolean {
+    if (typeof value !== 'string') {
+        return false;
+    }
+
+    return !isNaN(value as any) && !isNaN(parseFloat(value));
+}
