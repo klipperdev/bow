358381a4643b53d4c2b44a96067a7e0a854a68e6
François Pluchino <francois.pluchino@klipper.dev>
2020-11-11 01:14:14 +0100

    Add standard view field text component

---
+++ components/KStandardViewFieldText/KStandardViewFieldText.vue
@@ -0,0 +1,43 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KStandardViewFieldText.ts" />
+
+<template>
+    <k-col-label
+        v-bind="genColLabelProps"
+        v-on="genColLabelListeners"
+    >
+        <template v-slot:view>
+            <k-text
+                :value="fieldValue || defaultValue"
+            >
+                <template v-for="slotItem in getSlotItems('view')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-text>
+        </template>
+
+        <template v-slot:edit="scope">
+            <k-form-text
+                v-model="fieldValue"
+                v-bind="genEditProps"
+                v-on="genEditListeners"
+            >
+                <template v-for="slotItem in getSlotItems('form')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-form-text>
+        </template>
+
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-col-label>
+</template>
