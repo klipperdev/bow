c54168418bbec71cf7b4fc4607c335b1bb0334b2
François Pluchino <francois.pluchino@klipper.dev>
2020-11-11 22:48:17 +0100

    Add link association component

---
+++ components/KLinkAssociation/KLinkAssociation.vue
@@ -0,0 +1,29 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KLinkAssociation.ts" />
+
+<template>
+    <router-link
+        v-if="!!genTo"
+        v-bind="$attrs"
+        v-on="$listeners"
+        :to="genTo"
+    >
+        {{ genLabel }}
+    </router-link>
+
+    <span
+        v-else
+        v-bind="$attrs"
+        v-on="$listeners"
+    >
+        {{ genLabel }}
+    </span>
+</template>
