3de58dc9c8b7244aa51030d58b3eaedce0262de1
François Pluchino <francois.pluchino@klipper.dev>
2020-11-11 13:18:59 +0100

    Add form email component

---
+++ components/KFormEmail/KFormEmail.vue
@@ -0,0 +1,21 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KFormEmail.ts" />
+
+<template>
+    <k-form-text
+        v-bind="genProps"
+        v-on="$listeners"
+    >
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-form-text>
+</template>
