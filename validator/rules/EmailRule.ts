81d35cd3e5d96c6d04fe0e162cb39578b623c71d
François Pluchino <francois.pluchino@klipper.dev>
2020-11-11 17:59:03 +0100

    Optimise the email regex pattern

--- validator/rules/EmailRule.ts
+++ validator/rules/EmailRule.ts
@@ -22,7 +22,7 @@ export class EmailRule extends BaseRule {
             return true;
         }
 
-        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         return re.test(value) || this.getMessage('This value is not an address email');
     }
