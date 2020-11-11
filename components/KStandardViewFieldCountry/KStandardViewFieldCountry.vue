c8c537027074bb2ee4f369aee55aea3dfb02f8e9
François Pluchino <francois.pluchino@klipper.dev>
2020-11-11 01:50:58 +0100

    Add standard view field country component

---
+++ components/KStandardViewFieldCountry/KStandardViewFieldCountry.vue
@@ -0,0 +1,44 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KStandardViewFieldCountry.ts" />
+
+<template>
+    <k-col-label
+        v-bind="genColLabelProps"
+        v-on="genColLabelListeners"
+    >
+        <template v-slot:view>
+            <k-country
+                :value="fieldValue"
+                :defaultValue="defaultValue"
+            >
+                <template v-for="slotItem in getSlotItems('view')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-country>
+        </template>
+
+        <template v-slot:edit>
+            <k-form-country
+                v-model="fieldValue"
+                v-bind="genEditProps"
+                v-on="genEditListeners"
+            >
+                <template v-for="slotItem in getSlotItems('form')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-form-country>
+        </template>
+
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-col-label>
+</template>
