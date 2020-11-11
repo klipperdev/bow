2c57eb5b6a5d05c784b31aab9369b503a933f33d
François Pluchino <francois.pluchino@klipper.dev>
2020-11-11 01:14:47 +0100

    Add standard view field textarea component

---
+++ components/KStandardViewFieldTextarea/KStandardViewFieldTextarea.ts
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {SlotWrapper} from '@klipper/bow/mixins/SlotWrapper';
+import {Component} from 'vue-property-decorator';
+import {mixins} from 'vue-class-component';
+import {StandardViewFieldable} from '@klipper/bow/mixins/StandardViewFieldable';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+@Component
+export default class KStandardViewFieldTextarea extends mixins(
+    StandardViewFieldable,
+    SlotWrapper,
+) {
+}
