63b710771e5bfac4572ee6a03d11df332b3d7541
François Pluchino <francois.pluchino@klipper.dev>
2020-11-12 00:01:29 +0100

    Add standard view section system component

---
+++ components/KStandardViewSectionSystem/KStandardViewSectionSystem.ts
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {Dictionary} from '@klipper/bow/generic/Dictionary';
+import {SlotWrapper} from '@klipper/bow/mixins/SlotWrapper';
+import {StandardViewItem} from '@klipper/bow/mixins/StandardViewItem';
+import {mixins} from 'vue-class-component';
+import {Component} from 'vue-property-decorator';
+
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+@Component
+export default class KStandardViewSectionSystem extends mixins(
+    StandardViewItem,
+    SlotWrapper,
+) {
+    protected get genProps(): Dictionary<any> {
+        return Object.assign({
+            locked: this.standardData.editMode || undefined !== this.$attrs.locked,
+            loading: this.standardData.loading,
+            metadata: this.standardData.metadata,
+            data: this.standardData.data,
+        }, this.$attrs);
+    }
+}
---
+++ components/KStandardViewSectionSystem/KStandardViewSectionSystem.vue
@@ -0,0 +1,22 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KStandardViewSectionSystem.ts" />
+
+<template>
+    <k-card-section-system
+        v-if="!standardData.isCreate"
+        v-bind="genProps"
+        v-on="$listeners"
+    >
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-card-section-system>
+</template>
