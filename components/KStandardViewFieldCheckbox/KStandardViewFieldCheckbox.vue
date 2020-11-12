70ad98df15778ce0dddc8032e645aedc4854e3fb
François Pluchino <francois.pluchino@klipper.dev>
2020-11-12 22:03:20 +0100

    Add standard view field checkbox component

---
+++ components/KStandardViewFieldCheckbox/KStandardViewFieldCheckbox.vue
@@ -0,0 +1,48 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KStandardViewFieldCheckbox.ts" />
+
+<template>
+    <k-col-label
+        v-bind="genColLabelProps"
+        v-on="genColLabelListeners"
+    >
+        <template v-slot:read>
+            <k-checkbox
+                ref="read"
+                v-bind="genViewProps"
+                v-on="genViewListeners"
+                :value="fieldValue"
+            >
+                <template v-for="slotItem in getSlotItems('view')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-checkbox>
+        </template>
+
+        <template v-slot:edit>
+            <v-checkbox
+                ref="edit"
+                v-model="fieldValue"
+                v-bind="genEditProps"
+                v-on="genEditListeners"
+                class="ma-0"
+            >
+                <template v-for="slotItem in getSlotItems('form')" v-slot:[slotItem.target]>
+                    <slot :name="slotItem.original"/>
+                </template>
+            </v-checkbox>
+        </template>
+
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-col-label>
+</template>
