43f42606c4991406bff4f3fa0a781c65c6325092
François Pluchino <francois.pluchino@klipper.dev>
2020-11-12 17:55:39 +0100

    Add number component

---
+++ components/KNumber/KNumber.ts
@@ -0,0 +1,59 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {SlotWrapper} from '@klipper/bow/mixins/SlotWrapper';
+import {mixins} from 'vue-class-component';
+import {Component, Prop} from 'vue-property-decorator';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+@Component
+export default class KNumber extends mixins(
+    SlotWrapper,
+) {
+    @Prop({type: String, default: 'number', validator: (value: string) => {
+        return -1 !== ['number', 'percent', 'currency'].indexOf(value);
+    }})
+    public type!: string;
+
+    @Prop({type: Number, default: undefined})
+    public scale!: number;
+
+    @Prop({type: [String, Boolean], default: undefined})
+    public currency!: string|boolean|undefined;
+
+    @Prop({type: Boolean, default: undefined})
+    public percent!: boolean;
+
+    @Prop({type: String, default: undefined})
+    public display!: string;
+
+    private get genValue(): string|undefined {
+        let type = this.percent ? 'percent' : this.type;
+        let currency = this.currency;
+
+        if (true === currency || '' === currency) {
+            type = 'currency';
+            currency = undefined;
+        } else if (false === currency) {
+            currency = undefined;
+        }
+
+        switch (type) {
+            case 'percent':
+                return this.$percent(this.$attrs.value, this.scale);
+            case 'currency':
+                return this.$currency(this.$attrs.value, this.scale, currency, this.display);
+            case 'number':
+            default:
+                return this.$number(this.$attrs.value, this.scale);
+        }
+    }
+}
