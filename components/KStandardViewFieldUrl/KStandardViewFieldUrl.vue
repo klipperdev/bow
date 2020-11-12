78c774c011c36c4551269f555de85d9376a2d858
François Pluchino <francois.pluchino@klipper.dev>
2020-11-12 12:33:40 +0100

    Add standard view field url component

---
+++ components/KStandardViewFieldUrl/KStandardViewFieldUrl.vue
@@ -0,0 +1,48 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KStandardViewFieldUrl.ts" />
+
+<template>
+    <k-col-label
+        v-bind="genColLabelProps"
+        v-on="genColLabelListeners"
+    >
+        <template v-slot:read>
+            <k-link-url
+                ref="read"
+                v-bind="genViewProps"
+                v-on="genViewListeners"
+                :src="fieldValue"
+                :default-value="defaultValue"
+            >
+                <template v-for="slotItem in getSlotItems('view')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-link-url>
+        </template>
+
+        <template v-slot:edit>
+            <k-form-url
+                ref="edit"
+                v-model="fieldValue"
+                v-bind="genFieldEditProps"
+                v-on="genEditListeners"
+            >
+                <template v-for="slotItem in getSlotItems('form')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-form-url>
+        </template>
+
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-col-label>
+</template>
