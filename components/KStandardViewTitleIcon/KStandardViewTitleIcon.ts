6a054e0f85a22e11a75c43112602eae512a7d2f3
François Pluchino <francois.pluchino@klipper.dev>
2020-11-15 00:53:58 +0100

    Update color of principal icons

--- components/KStandardViewTitleIcon/KStandardViewTitleIcon.ts
+++ components/KStandardViewTitleIcon/KStandardViewTitleIcon.ts
@@ -20,9 +20,9 @@ export default class KStandardViewTitleIcon extends Vue {
     @Prop({type: Number, default: 30})
     public iconSize: number;
 
-    @Prop({type: String, default: 'primary'})
+    @Prop({type: String, default: 'accent'})
     public color: string;
 
-    @Prop({type: String, default: 'primary lighten-3'})
+    @Prop({type: String, default: 'accent lighten-1'})
     public darkColor: string;
 }
