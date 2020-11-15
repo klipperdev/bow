f19b6f98eee9ef22bd0636713dbcc7936c6b2512
François Pluchino <francois.pluchino@klipper.dev>
2020-11-15 16:36:34 +0100

    Add date component

---
+++ components/KDate/KDate.vue
@@ -0,0 +1,22 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KDate.ts" />
+
+<template>
+    <k-datetime
+        v-bind="$attrs"
+        v-on="$listeners"
+        type="date"
+    >
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-datetime>
+</template>
