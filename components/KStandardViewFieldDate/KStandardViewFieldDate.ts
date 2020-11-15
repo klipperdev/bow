4eff81a0f84a1d1078ba2b8a236f09cff399a95d
François Pluchino <francois.pluchino@klipper.dev>
2020-11-15 19:23:15 +0100

    Add standard view field date component

---
+++ components/KStandardViewFieldDate/KStandardViewFieldDate.ts
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {SlotWrapper} from '@klipper/bow/mixins/SlotWrapper';
+import {mixins} from 'vue-class-component';
+import {Component} from 'vue-property-decorator';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+@Component
+export default class KStandardViewFieldDate extends mixins(
+    SlotWrapper,
+) {
+}
