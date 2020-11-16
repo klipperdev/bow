1053ce654f30417d9039b989b34563ef39d800e8
François Pluchino <francois.pluchino@klipper.dev>
2020-11-16 23:06:47 +0100

    Add standard view field system choice component

---
+++ components/KStandardViewFieldSystemChoice/KStandardViewFieldSystemChoice.vue
@@ -0,0 +1,49 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KStandardViewFieldSystemChoice.ts" />
+
+<template>
+    <k-col-label
+        v-bind="genColLabelProps"
+        v-on="genColLabelListeners"
+    >
+        <template v-slot:read>
+            <k-choice-system
+                v-if="!isMultiple"
+                ref="read"
+                v-bind="genViewProps"
+                v-on="genViewListeners"
+                :type="fieldSystemChoiceType"
+                :choice="fieldValue"
+            >
+                <template v-for="slotItem in getSlotItems('view')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-choice-system>
+        </template>
+
+        <template v-slot:edit>
+            <k-form-system-choice
+                ref="edit"
+                v-model="fieldValue"
+                v-bind="genSystemChoiceEditProps"
+                v-on="genEditListeners"
+            >
+                <template v-for="slotItem in getSlotItems('form')" v-slot:[slotItem.target]="props">
+                    <slot :name="slotItem.original" v-bind="props"/>
+                </template>
+            </k-form-system-choice>
+        </template>
+
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-col-label>
+</template>
