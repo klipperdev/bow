fb8a83b6099ed4d0b0c823afbd1c88dad2f20f11
François Pluchino <francois.pluchino@klipper.dev>
2020-11-16 23:06:12 +0100

    Add choice system component

---
+++ components/KChoiceSystem/KChoiceSystem.ts
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {SystemChoice} from '@klipper/bow/metadata/SystemChoice';
+import {SlotWrapper} from '@klipper/bow/mixins/SlotWrapper';
+import {mixins} from 'vue-class-component';
+import {Component, Prop} from 'vue-property-decorator';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+@Component
+export default class KChoiceSystem extends mixins(
+    SlotWrapper,
+) {
+    @Prop({type: String})
+    public type!: string|undefined;
+
+    private get genValue(): any {
+        const value = this.$attrs.choice;
+
+        if (!!value
+                && !!this.$store.state.metadata.systemChoices
+                && !!this.$store.state.metadata.systemChoices[this.type]) {
+            const choices = this.$store.state.metadata.systemChoices[this.type] as SystemChoice[];
+
+            for (const choice of choices) {
+                if (value === choice.value) {
+                    return choice;
+                }
+            }
+        }
+
+        return value;
+    }
+}
---
+++ components/KChoiceSystem/KChoiceSystem.vue
@@ -0,0 +1,22 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KChoiceSystem.ts" />
+
+<template>
+    <k-choice-chip
+        v-bind="$attrs"
+        v-on="$listeners"
+        :choice="genValue"
+    >
+        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
+            <slot :name="slotItem.original" v-bind="props"/>
+        </template>
+    </k-choice-chip>
+</template>
