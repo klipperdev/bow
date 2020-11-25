eb0dddf51d37d39ead99f70bd822c1b4e67b966d
François Pluchino <francois.pluchino@klipper.dev>
2020-11-25 14:50:01 +0100

    Add standard component component

---
+++ components/KStandardComponent/KStandardComponent.vue
@@ -0,0 +1,35 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KStandardComponent.ts" />
+
+<template>
+    <k-loader-wrapper
+        v-bind="$attrs"
+        v-on="$listeners"
+        :loading="fetchLoading"
+        @keydown="onKeyDown"
+    >
+        <slot
+            name="form"
+            v-bind="genSlotProps"
+        >
+            <v-form
+                ref="form"
+                @submit.prevent=""
+                autocomplete="off"
+            >
+                <slot
+                    name="default"
+                    v-bind="genSlotProps"
+                />
+            </v-form>
+        </slot>
+    </k-loader-wrapper>
+</template>
