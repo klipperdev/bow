c18da06f7a9c6bfe6c3c9224c78140f35bde3741
François Pluchino <francois.pluchino@klipper.dev>
2020-11-29 17:02:32 +0100

    Add form color component

---
+++ components/KFormColor/KFormColor.ts
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {Component, Vue} from 'vue-property-decorator';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+@Component({
+    inheritAttrs: false,
+})
+export default class KFormColor extends Vue {
+}
---
+++ components/KFormColor/KFormColor.vue
@@ -0,0 +1,21 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KFormColor.ts" />
+
+<template>
+    <v-color-picker
+        ref="color"
+        v-bind="$attrs"
+        v-on="$listeners"
+        :dot-size="undefined === $attrs['dot-size'] ? 24 : $attrs['dot-size']"
+        :hide-mode-switch="undefined === $attrs['hide-mode-switch'] ? true : $attrs['hide-mode-switch']"
+        :mode="undefined === $attrs['mode'] ? 'hexa' : $attrs['mode']"
+    />
+</template>
