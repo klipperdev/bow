ef0e2578a6905c9bef539f4c2d7bd150b690a903
François Pluchino <francois.pluchino@klipper.dev>
2020-11-29 17:03:22 +0100

    Add icon component

---
+++ components/KIcon/KIcon.vue
@@ -0,0 +1,27 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KIcon.ts" />
+
+<template>
+    <v-icon
+        v-if="!!value"
+        v-bind="genProps"
+        v-on="$listeners"
+    >
+        <slot name="default">
+            {{ value || defaultValue }}
+        </slot>
+    </v-icon>
+
+    <k-text
+        v-else
+        :value="defaultValue"
+    />
+</template>
