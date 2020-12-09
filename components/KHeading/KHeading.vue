12eff6c4c390e0e19079488c22f0541568774341
François Pluchino <francois.pluchino@klipper.dev>
2020-12-09 05:44:10 +0100

    Add heading component

---
+++ components/KHeading/KHeading.vue
@@ -0,0 +1,23 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KHeading.ts" />
+
+<style lang="scss" src="./KHeading.scss" />
+
+<template>
+    <v-container
+        v-bind="$attrs"
+        v-on="$listeners"
+        :class="classes"
+        fluid
+    >
+        <slot name="default"/>
+    </v-container>
+</template>
