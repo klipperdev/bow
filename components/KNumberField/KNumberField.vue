eef5afe68c1b2d127392978516c5e2b120a7dc26
François Pluchino <francois.pluchino@klipper.dev>
2020-12-21 18:10:41 +0100

    Add ref on text field of number field component

--- components/KNumberField/KNumberField.vue
+++ components/KNumberField/KNumberField.vue
@@ -11,6 +11,7 @@ file that was distributed with this source code.
 
 <template>
     <v-text-field
+        ref="text"
         v-bind="textAttrs"
         v-on="textListeners"
         v-model="formattedValue"
