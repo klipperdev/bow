5c541c11dc2de7cedb90455a5650f7313167696b
François Pluchino <francois.pluchino@klipper.dev>
2021-02-08 00:55:44 +0100

    Add standard import button component

---
+++ components/KStandardHeaderButtonImport/KStandardHeaderButtonImport.vue
@@ -0,0 +1,17 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts" src="./KStandardHeaderButtonImport.ts" />
+
+<template>
+    <k-import-action
+        v-bind="genButtonProps"
+        v-on="$listeners"
+    />
+</template>
