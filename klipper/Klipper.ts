b41b61344c3cb107a805e49aa1575a17dad5dc6b
François Pluchino <francois.pluchino@klipper.dev>
2021-07-30 14:01:32 +0200

    Update default limit of kanban item per page

--- klipper/Klipper.ts
+++ klipper/Klipper.ts
@@ -41,7 +41,7 @@ export class Klipper {
         userContextRedirectRoute?: Location,
         itemsPerPage: number[] = [10, 20, 50, 100],
         defaultItemPerPage: number = 20,
-        defaultKanbanItemPerPage: number = 100,
+        defaultKanbanItemPerPage: number = 50,
         defaultExportFormats: string[] = ['csv', 'html', 'xls', 'xlsx'],
         defaultImportFormats: string[] = ['csv', 'xls', 'xlsx'],
     ) {
