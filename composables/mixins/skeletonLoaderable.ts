d3958a09d539ec14b4274cb248906d415c4d5022
François Pluchino <francois.pluchino@klipper.dev>
2022-05-22 14:10:22 +0200

    Add skeleton loaderable mixin to use with composable api

---
+++ composables/mixins/skeletonLoaderable.ts
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {Dictionary} from '@klipper/bow/generic/Dictionary';
+import {randomNumberBetween} from '@klipper/bow/utils/number';
+import Vue, {ComponentOptions, PropType} from 'vue';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export const SkeletonLoaderable: ComponentOptions<Vue|any> = {
+    name: 'skeletonLoaderable',
+
+    props: {
+        loading: {
+            type: Boolean as PropType<boolean>,
+            default: false,
+        },
+
+        skeletonLoaderProps: {
+            type: Object as PropType<Dictionary<any>|undefined>,
+        },
+
+        disableLoading: {
+            type: Boolean as PropType<boolean>,
+            default: false,
+        },
+
+        /**
+         * Content width average used to create the skeleton loader
+         */
+        contentWidth: {
+            type: String as PropType<string>,
+            default: 'random',
+        },
+    },
+
+    computed: {
+        skeletonLoaderPropsValue(): Dictionary<any> {
+            let contentWidth = this.contentWidth || undefined;
+
+            if ('random' === contentWidth) {
+                contentWidth = randomNumberBetween(30, 80) + '%';
+            }
+
+            return Object.assign(
+                {class: 'mt-1', type: 'text', width: contentWidth},
+                this.skeletonLoaderProps || {},
+            );
+        },
+    },
+};
