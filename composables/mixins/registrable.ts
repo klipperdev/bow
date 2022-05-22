3f01c269fe05013df0bb78294204af9710d9e50b
François Pluchino <francois.pluchino@klipper.dev>
2022-05-22 14:01:28 +0200

    Add registrable mixin to use with composable api

---
+++ composables/mixins/registrable.ts
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export * from '@klipper/bow/mixins/Registrable';
