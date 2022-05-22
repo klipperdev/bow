06c7595a4a18092c676e7c8ee9fb1e9047f6df84
François Pluchino <francois.pluchino@klipper.dev>
2022-05-22 11:45:35 +0200

    Add app fill heightable mixin to use with composable api

---
+++ composables/mixins/appFillHeightable.ts
@@ -0,0 +1,60 @@
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import Vue, {ComponentOptions} from 'vue';
+import {Component} from 'vue-property-decorator';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export const appFillHeightable: ComponentOptions<Vue|any> = {
+    created(): void {
+        if (0 === initializedCount) {
+            const htmlEl = getHtmlElement();
+
+            if (null !== htmlEl) {
+                htmlEl.classList.add('app-fill-height');
+            }
+        }
+
+        ++initializedCount;
+    },
+
+    destroyed(): void {
+        --initializedCount;
+
+        if (0 === initializedCount) {
+            const htmlEl = getHtmlElement();
+
+            if (null !== htmlEl) {
+                htmlEl.classList.remove('app-fill-height');
+            }
+        }
+    },
+
+    computed: {
+        initializedCount: function () {
+            return initializedCount;
+        }
+    },
+}
+
+/**
+ * Global helper.
+ */
+let initializedCount: number = 0
+let htmlEl: HTMLElement|null = null;
+
+function getHtmlElement(): HTMLElement|null {
+    if (null === htmlEl) {
+        htmlEl = document.querySelector('html');
+    }
+
+    return htmlEl;
+}
