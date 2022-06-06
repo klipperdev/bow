331c4f415ff99caec704c64cb01798ea38b48aae
François Pluchino <francois.pluchino@klipper.dev>
2022-06-06 14:24:54 +0200

    Convert KAddress component to composable api

--- components/KAddress/KAddress.vue
+++ components/KAddress/KAddress.vue
@@ -7,110 +7,118 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 -->
 
-<template>
-    <k-textarea
-        v-bind="getProps"
-        v-on="$listeners"
-    >
-        <template v-for="slotItem in getSlotItems('')" v-slot:[slotItem.target]="props">
-            <slot :name="slotItem.original" v-bind="props"/>
-        </template>
-    </k-textarea>
-</template>
-
 <script lang="ts">
-import {Dictionary} from '@klipper/bow/generic/Dictionary';
-import {SlotWrapper} from '@klipper/bow/mixins/SlotWrapper';
-import {mixins} from 'vue-class-component';
-import {Component, Prop} from 'vue-property-decorator';
+import KTextarea from '@klipper/bow/components/KTextarea/KTextarea.vue';
+import {defineComponent} from '@vue/composition-api';
+import {PropType} from 'vue';
+
 
 /**
  * @author François Pluchino <francois.pluchino@klipper.dev>
  */
-@Component
-export default class KAddress extends mixins(
-    SlotWrapper,
-) {
-    @Prop({required: true, default: null})
-    public value!: Dictionary<any>|null;
+export default defineComponent({
+    name: 'KAddress',
 
-    @Prop({type: Boolean, default: false})
-    public hideStreet!: boolean;
+    extends: KTextarea,
 
-    @Prop({type: Boolean, default: false})
-    public hideStreetComplement!: boolean;
+    props: {
+        value: {
+            type: [Object, null] as PropType<Dictionary<any>|null>,
+            default: null,
+            required: true,
+        },
 
-    @Prop({type: Boolean, default: false})
-    public hidePostalCode!: boolean;
+        hideStreet: {
+            type: Boolean,
+            default: false,
+        },
 
-    @Prop({type: Boolean, default: false})
-    public hideCity!: boolean;
+        hideStreetComplement: {
+            type: Boolean,
+            default: false,
+        },
 
-    @Prop({type: Boolean, default: false})
-    public hideState!: boolean;
+        hidePostalCode: {
+            type: Boolean,
+            default: false,
+        },
 
-    @Prop({type: Boolean, default: false})
-    public hideCountry!: boolean;
+        hideCity: {
+            type: Boolean,
+            default: false,
+        },
 
-    @Prop({type: Boolean, default: false})
-    public inline!: boolean;
+        hideState: {
+            type: Boolean,
+            default: false,
+        },
 
-    protected get classes(): Dictionary<boolean> {
-        return {
-            'k-address': true,
-        };
-    }
+        hideCountry: {
+            type: Boolean,
+            default: false,
+        },
 
-    private get genValue(): string|undefined {
-        let address = '';
+        inline: {
+            type: Boolean,
+            default: false,
+        },
+    },
 
-        if (typeof this.value === 'object') {
-            const street = this.value?.street;
-            const streetComplement = this.value?.street_complement;
-            const postalCode = this.value?.postal_code;
-            const city = this.value?.city;
-            const state = this.value?.state;
-            const country = this.value?.country;
+    computed: {
+        classes(): object {
+            return {
+                'k-address': true,
+                ...this.themeClasses,
+                ...this.textColorClasses,
+            };
+        },
+    },
 
-            if (street && !this.hideStreet) {
-                address += street;
-            }
+    methods: {
+        beforeGenValue(value?: string): string|undefined {
+            let address = '';
 
-            if (streetComplement && !this.hideStreetComplement) {
-                address += (address ? '\n' : '') + streetComplement;
-            }
+            if (typeof this.value === 'object') {
+                const street = this.value?.street;
+                const streetComplement = this.value?.street_complement;
+                const postalCode = this.value?.postal_code;
+                const city = this.value?.city;
+                const state = this.value?.state;
+                const country = this.value?.country;
 
-            if (postalCode && !this.hidePostalCode) {
-                address += (address ? '\n' : '') + postalCode;
-
-                if (city) {
-                    address += ' ' + city;
+                if (street && !this.hideStreet) {
+                    address += street;
                 }
-            } else if (city && !this.hideCity) {
-                address += (address ? '\n' : '') + city;
+
+                if (streetComplement && !this.hideStreetComplement) {
+                    address += (address ? '\n' : '') + streetComplement;
+                }
+
+                if (postalCode && !this.hidePostalCode) {
+                    address += (address ? '\n' : '') + postalCode;
+
+                    if (city) {
+                        address += ' ' + city;
+                    }
+                } else if (city && !this.hideCity) {
+                    address += (address ? '\n' : '') + city;
+                }
+
+                if (state && !this.hideState) {
+                    address += (address ? '\n' : '') + state;
+                }
+
+                if (country && !this.hideCountry) {
+                    address += (address ? '\n' : '') + this.$country(country);
+                }
             }
 
-            if (state && !this.hideState) {
-                address += (address ? '\n' : '') + state;
+            if (this.inline && address) {
+                return address.replaceAll('\n', ' ');
             }
 
-            if (country && !this.hideCountry) {
-                address += (address ? '\n' : '') + this.$country(country);
-            }
-        }
-
-        if (this.inline && address) {
-            return address.replaceAll('\n', ' ');
-        }
-
-        return address || undefined;
-    }
-
-    private get getProps(): Dictionary<any> {
-        return Object.assign({
-            class: this.classes,
-            value: this.genValue,
-        }, this.$attrs);
-    }
-}
+            return address || undefined;
+        },
+    },
+});
 </script>
