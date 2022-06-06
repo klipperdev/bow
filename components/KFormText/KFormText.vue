b53162c95a7bb4ca6b4f9b7462a89f0fd4d17728
François Pluchino <francois.pluchino@klipper.dev>
2022-06-06 13:09:23 +0200

    Extend VTextField for KFormText component

--- components/KFormText/KFormText.vue
+++ components/KFormText/KFormText.vue
@@ -7,33 +7,16 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 -->
 
-<template>
-    <v-text-field
-        ref="text"
-        v-bind="$attrs"
-        v-on="$listeners"
-        :filled="undefined === $attrs.filled ? true : $attrs.filled"
-    >
-        <template v-slot:append><slot name="append"/></template>
-        <template v-slot:append-outer><slot name="append-outer"/></template>
-        <template v-slot:default><slot name="default"/></template>
-        <template v-slot:label><slot name="label"/></template>
-        <template v-slot:message="scope"><slot name="message" v-bind="scope"/></template>
-        <template v-slot:prepend><slot name="prepend"/></template>
-        <template v-slot:prepend-inner><slot name="prepend-inner"/></template>
-        <template v-slot:progress><slot name="progress"/></template>
-    </v-text-field>
-</template>
-
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
+import {defineComponent} from '@vue/composition-api';
+import {VTextField} from 'vuetify/lib';
 
 /**
  * @author François Pluchino <francois.pluchino@klipper.dev>
  */
-@Component({
-    inheritAttrs: false,
-})
-export default class KFormText extends Vue {
-}
+export default defineComponent({
+    name: 'KFormText',
+
+    extends: VTextField,
+});
 </script>
