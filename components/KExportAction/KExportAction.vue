618874750f4d97020e156e9c067577b366b8351d
François Pluchino <francois.pluchino@klipper.dev>
2022-06-08 15:14:36 +0200

    Use KComponentExportAction component in KExportAction

--- components/KExportAction/KExportAction.vue
+++ components/KExportAction/KExportAction.vue
@@ -8,198 +8,165 @@ file that was distributed with this source code.
 -->
 
 <template>
-    <v-btn
-        v-bind="$attrs"
-        v-on="$listeners"
-        :id="'exportAction_' + self._uid"
-        :loading="loading"
-        :icon="btnIcon"
-        @click="onClickButton"
+    <k-component-export-action
+        ref="export"
+        :metadata="metadata"
+        :formats="formats"
+        :sort="sort"
+        :filter="filter"
+        :search="search"
+        :fields="fields"
+        v-slot="exportProps"
     >
-        <slot name="icon">
-            <v-icon v-bind="genIconProps">{{ icon }}</v-icon>
-        </slot>
-
-        <v-icon
-            v-if="!isSingle"
-            v-bind="genIconProps"
+        <v-btn
+            v-bind="$attrs"
+            v-on="$listeners"
+            :id="'exportAction_' + _uid"
+            :loading="exportProps.exportLoading"
+            :icon="btnIcon"
+            @click="onClickButton"
         >
-            arrow_drop_down
-        </v-icon>
+            <slot name="icon">
+                <v-icon v-bind="genIconProps">{{ icon }}</v-icon>
+            </slot>
 
-        <v-menu
-            v-if="!isSingle"
-            v-bind="genMenuProps"
-            v-model="open"
-            :activator="'#exportAction_' + self._uid"
-        >
-            <v-list dense>
-                <v-list-item
-                    v-for="format in formats"
-                    :key="format"
-                    @click="exportAction(format)"
-                >
-                    <v-list-item-title>
-                        <v-icon small left>{{ getFormatIcon(format) }}</v-icon>
-                        {{ $t('export.format', {'format': getFormatLabel(format)}) }}
-                    </v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-menu>
-    </v-btn>
+            <v-icon
+                v-if="!exportProps.isSingle"
+                v-bind="genIconProps"
+            >
+                arrow_drop_down
+            </v-icon>
+
+            <v-menu
+                v-if="!exportProps.isSingle"
+                v-bind="genMenuProps"
+                v-model="open"
+                :activator="'#exportAction_' + _uid"
+            >
+                <v-list dense>
+                    <v-list-item
+                        v-for="format in exportProps.formats"
+                        :key="format"
+                        @click="exportProps.exportAction(format)"
+                    >
+                        <v-list-item-title>
+                            <v-icon small left>{{ exportProps.getFormatIcon(format) }}</v-icon>
+                            {{ exportProps.getLabel(format) }}
+                        </v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-btn>
+    </k-component-export-action>
 </template>
 
 <script lang="ts">
+import {AjaxContent} from '@klipper/bow/composables/mixins/http/ajaxContent';
 import {Dictionary} from '@klipper/bow/generic/Dictionary';
-import {AjaxContent} from '@klipper/bow/mixins/http/AjaxContent';
-import {Selfable} from '@klipper/bow/mixins/Selfable';
-import {FilterCondition} from '@klipper/sdk/models/filters/FilterCondition';
-import {FilterRule} from '@klipper/sdk/models/filters/FilterRule';
 import {Sort} from '@klipper/sdk/requests/Sort';
-import {mixins} from 'vue-class-component';
-import {Component, Prop} from 'vue-property-decorator';
+import {defineComponent, PropType} from '@vue/composition-api';
 
 /**
  * @author François Pluchino <francois.pluchino@klipper.dev>
  */
-@Component
-export default class KDeleteAction extends mixins(
-    AjaxContent,
-    Selfable,
-) {
-    @Prop({type: String})
-    public metadata!: string;
+export default defineComponent({
+    name: 'KDeleteAction',
 
-    @Prop({type: String})
-    public exportEndpoint!: string;
+    mixins: [
+        AjaxContent,
+    ],
 
-    @Prop({type: String, default: 'fa-fw fa-file-download'})
-    public icon!: string;
+    props: {
+        metadata: {
+            type: String,
+            default: undefined,
+        },
 
-    @Prop({type: Boolean, default: false})
-    public btnIcon!: boolean;
+        exportEndpoint: {
+            type: String,
+            default: undefined,
+        },
 
-    @Prop({type: Array, default() {
-        return this.$klipper.defaultExportFormats;
-    }})
-    public formats!: string[];
+        icon: {
+            type: String,
+            default: 'fa-fw fa-file-download',
+        },
 
-    @Prop({type: Object, default: () => {}})
-    public menuProps!: Dictionary<any>;
+        btnIcon: {
+            type: Boolean,
+            default: false,
+        },
 
-    @Prop({default: undefined})
-    public sort!: string|string[]|Sort|Sort[]|undefined;
+        formats: {
+            type: Array as PropType<string[]>,
+            default() {
+                return this.$klipper.defaultExportFormats;
+            },
+        },
 
-    @Prop({default: undefined})
-    public filter!: FilterCondition|FilterRule|undefined;
+        menuProps: {
+            type: Object as PropType<Dictionary<any>>,
+            default: () => ({}),
+        },
 
-    @Prop({type: String, default: undefined})
-    public search!: string|undefined;
+        sort: {
+            type: [String, Array] as PropType<string|string[]|Sort|Sort[]|undefined>,
+            default: undefined,
+        },
 
-    @Prop({type: Array, default: undefined})
-    public fields!: string[]|undefined;
+        filter: {
+            type: Object as PropType<Filter|undefined>,
+            default: undefined,
+        },
 
-    private open: boolean = false;
+        search: {
+            type: String,
+            default: undefined,
+        },
 
-    private get genIconProps(): Dictionary<any> {
+        fields: {
+            type: Array as PropType<string[]|undefined>,
+            default: undefined,
+        },
+    },
+
+    data(): Dictionary<any> {
         return {
-            small: !!this.$attrs.small || '' === this.$attrs.small,
+            open: false as boolean,
         };
-    }
+    },
 
-    private get genMenuProps(): Dictionary<any> {
-        return Object.assign({
-            'open-on-click': false,
-            'transition': 'slide-y-transition',
-            'max-height': '90vh',
-            'offset-y': true,
-            'bottom': true,
-            'left': true,
-        }, this.menuProps);
-    }
+    computed: {
+        genIconProps(): Dictionary<any> {
+            return {
+                small: !!this.$attrs.small || '' === this.$attrs.small,
+            };
+        },
 
-    private get isSingle(): boolean {
-        return 1 === this.formats.length;
-    }
+        genMenuProps(): Dictionary<any> {
+            return Object.assign({
+                'open-on-click': false,
+                'transition': 'slide-y-transition',
+                'max-height': '90vh',
+                'offset-y': true,
+                'bottom': true,
+                'left': true,
+            }, this.menuProps);
+        },
+    },
 
-    public async export(): Promise<void> {
-        await this.onClickButton();
-    }
+    methods: {
+        async export(): Promise<void> {
+            await this.$refs.export.export();
+        },
 
-    protected getFormatLabel(format: string): string {
-        return format.toUpperCase();
-    }
-
-    protected getFormatIcon(format: string): string {
-        switch (format) {
-            case 'csv':
-                return 'fa-fw fa-file-csv';
-            case 'html':
-                return 'fa-fw fa-html5';
-            case 'pdf':
-                return 'fa-fw fa-pdf';
-            case 'xls':
-            case 'xlsx':
-                return 'fa-fw fa-file-excel';
-            default:
-                return 'fa-fw fa-file';
-        }
-    }
-
-    private async onClickButton(): Promise<void> {
-        if (this.isSingle) {
-            await this.exportAction(this.formats[0]);
-        } else {
-            this.open = true;
-        }
-    }
-
-    private async exportAction(format: string): Promise<void> {
-        const meta = await this.$metadata.get(this.metadata);
-
-        if (0 === this.formats.length || !this.formats.includes(format) || (!meta && !this.exportEndpoint)) {
-            return;
-        }
-
-        const exportUrl = !meta
-            ? this.exportEndpoint
-            : '/{organization}/' + meta.pluralName + '.{ext}';
-
-        const res = await this.fetchData(async (canceler) => {
-            return await this.$api.requestRaw<string>({
-                url: exportUrl.replaceAll('{ext}', format),
-                method: 'GET',
-                responseType: 'blob',
-                sort: this.sort || undefined,
-                filter: this.filter || undefined,
-                search: this.search || undefined,
-                fields: this.fields || undefined,
-            }, canceler);
-        });
-
-        if (null !== res) {
-            let filename = 'export.' + format;
-            const disposition = res.request.getResponseHeader('Content-Disposition');
-
-            if (disposition && disposition.indexOf('attachment') !== -1) {
-                const filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-                const matches = filenameRegex.exec(disposition);
-
-                if (matches != null && matches[1]) {
-                    filename = matches[1].replace(/['"]/g, '');
-                }
+        async onClickButton(): Promise<void> {
+            if (this.$refs.export.isSingle) {
+                await this.$refs.export.export(this.$refs.export.formats[0]);
+            } else {
+                this.open = true;
             }
-
-
-            const fileURL = window.URL.createObjectURL(new Blob([res.data]));
-            const fileLink = document.createElement('a');
-
-            fileLink.href = fileURL;
-            fileLink.setAttribute('download', filename);
-            document.body.appendChild(fileLink);
-
-            fileLink.click();
-        }
-    }
-}
+        },
+    },
+});
 </script>
