1902ea575b1f8722c8fd7ca1308aa675fec17cd2
François Pluchino <francois.pluchino@klipper.dev>
2022-06-21 19:34:03 +0200

    Allow to access on referenced scoped component in formable mixins

--- composables/mixins/formable.ts
+++ composables/mixins/formable.ts
@@ -20,81 +20,81 @@ export function formable(componentRef: string): VueConstructor {
 
         computed: {
             hasError(): boolean {
-                return this.$refs[componentRef]?.hasError || false;
+                return getRefComponent(this, componentRef)?.hasError || false;
             },
 
             hasSuccess(): boolean {
-                return this.$refs[componentRef]?.hasSuccess || false;
+                return getRefComponent(this, componentRef)?.hasSuccess || false;
             },
 
             externalError(): boolean {
-                return this.$refs[componentRef]?.externalError || false;
+                return getRefComponent(this, componentRef)?.externalError || false;
             },
 
             hasMessages(): boolean {
-                return this.$refs[componentRef]?.hasMessages || false;
+                return getRefComponent(this, componentRef)?.hasMessages || false;
             },
 
             hasState(): boolean {
-                return this.$refs[componentRef]?.hasState || false;
+                return getRefComponent(this, componentRef)?.hasState || false;
             },
 
             isDisabled(): boolean {
-                return this.$refs[componentRef]?.isDisabled || false;
+                return getRefComponent(this, componentRef)?.isDisabled || false;
             },
 
             isInteractive(): boolean {
-                return this.$refs[componentRef]?.isInteractive || false;
+                return getRefComponent(this, componentRef)?.isInteractive || false;
             },
 
             isReadonly(): boolean {
-                return this.$refs[componentRef]?.isReadonly || false;
+                return getRefComponent(this, componentRef)?.isReadonly || false;
             },
 
             shouldValidate(): boolean {
-                return this.$refs[componentRef]?.shouldValidate || false;
+                return getRefComponent(this, componentRef)?.shouldValidate || false;
             },
 
             validations(): InputValidationRules {
-                return this.$refs[componentRef]?.validations || [];
+                return getRefComponent(this, componentRef)?.validations || [];
             },
 
             validationState(): string|undefined {
-                return this.$refs[componentRef]?.validationState;
+                return getRefComponent(this, componentRef)?.validationState;
             },
 
             validationTarget(): InputValidationRules {
-                return this.$refs[componentRef]?.validationTarget || [];
+                return getRefComponent(this, componentRef)?.validationTarget || [];
             },
         },
 
         methods: {
             blur (e?: Event): void {
-                this.$refs[componentRef]?.blur(e);
+                getRefComponent(this, componentRef)?.blur(e);
             },
 
             focus(): void {
-                this.$refs[componentRef]?.focus();
+                getRefComponent(this, componentRef)?.focus();
             },
 
             reset(): void {
-                this.$refs[componentRef]?.reset();
+                getRefComponent(this, componentRef)?.reset();
             },
 
             clearableCallback(): void {
-                this.$refs[componentRef]?.clearableCallback();
+                getRefComponent(this, componentRef)?.clearableCallback();
             },
 
             validate(force = false, value?: any): boolean {
-                return this.$refs[componentRef]?.validate(force, value) || false;
+                return getRefComponent(this, componentRef)?.validate(force, value) || false;
             },
 
             resetValidation(): void {
-                const ref = this.$refs[componentRef]?.resetValidation();
+                const ref = getRefComponent(this, componentRef)?.resetValidation();
             },
 
             setValue(value?: any): void {
-                this.$refs[componentRef]?.setValue(value);
+                getRefComponent(this, componentRef)?.setValue(value);
             },
         },
     });
@@ -106,53 +106,53 @@ export function ajaxListFormable(componentRef: string): VueConstructor {
 
         computed: {
             page(): number {
-                return this.$refs[componentRef]?.page || 1;
+                return getRefComponent(this, componentRef)?.page || 1;
             },
 
             limit(): number {
-                return this.$refs[componentRef]?.limit || this.$klipper?.defaultItemPerPage || 20;
+                return getRefComponent(this, componentRef)?.limit || this.$klipper?.defaultItemPerPage || 20;
             },
 
             pages(): number {
-                return this.$refs[componentRef]?.pages || -1;
+                return getRefComponent(this, componentRef)?.pages || -1;
             },
 
             total(): number {
-                return this.$refs[componentRef]?.total || 0;
+                return getRefComponent(this, componentRef)?.total || 0;
             },
 
             search(): string {
-                return this.$refs[componentRef]?.search || '';
+                return getRefComponent(this, componentRef)?.search || '';
             },
         },
 
         methods: {
             cancel(): void {
-                this.$refs[componentRef]?.cancel();
+                getRefComponent(this, componentRef)?.cancel();
             },
 
             reset(): void {
-                this.$refs[componentRef]?.reset();
+                getRefComponent(this, componentRef)?.reset();
             },
 
             async previousPage(): Promise<void> {
-                await this.$refs[componentRef]?.previousPage();
+                await getRefComponent(this, componentRef)?.previousPage();
             },
 
             async nextPage(): Promise<void> {
-                await this.$refs[componentRef]?.nextPage();
+                await getRefComponent(this, componentRef)?.nextPage();
             },
 
             async refreshToFirstPage(): Promise<void> {
-                await this.$refs[componentRef]?.refreshToFirstPage();
+                await getRefComponent(this, componentRef)?.refreshToFirstPage();
             },
 
             deleteItem(value: string|number, key: string): number {
-                return this.$refs[componentRef]?.deleteItem(value, key) || 0;
+                return getRefComponent(this, componentRef)?.deleteItem(value, key) || 0;
             },
 
             async refresh(showSnackbar: boolean, topOnRefresh: boolean): Promise<void> {
-                await this.$refs[componentRef]?.refresh(showSnackbar, topOnRefresh);
+                await getRefComponent(this, componentRef)?.refresh(showSnackbar, topOnRefresh);
             },
         },
     });
@@ -169,20 +169,19 @@ export function ajaxSelectFormable(componentRef: string): VueConstructor {
 
         methods: {
             async initValue<T = any>(value: T|null): Promise<void> {
-                await this.$refs[componentRef]?.initValue(value);
+                await getRefComponent(this, componentRef)?.initValue(value);
             },
 
             setValue(value?: any): void {
-                this.$refs[componentRef]?.setValue(value);
+                getRefComponent(this, componentRef)?.setValue(value);
             },
 
             setValueByText(value?: any): void {
-                this.$refs[componentRef]?.setValueByText(value);
+                getRefComponent(this, componentRef)?.setValueByText(value);
             },
         },
     });
 };
-
 
 export function selectFormable(componentRef: string): VueConstructor {
     return Vue.extend({
@@ -192,6 +191,14 @@ export function selectFormable(componentRef: string): VueConstructor {
             formable(componentRef),
         ],
     });
+};
+
+const getRefComponent = function(self: Vue|any, componentRef: string): any|Vue|Element|(Vue|Element)[]|undefined {
+    if (self.$scopedSlots[componentRef]) {
+        return self.$scopedSlots[componentRef]()[0]?.context.$refs[componentRef] as any|Vue|Element|(Vue|Element)[]|undefined;
+    }
+
+    return self.$refs[componentRef];
 };
 
 const getRef = function(self: Vue, path?: string): any|Vue|Element|(Vue|Element)[]|undefined {
