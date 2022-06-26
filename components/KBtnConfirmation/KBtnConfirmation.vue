035e325b730ee37f66946240384dac6a1b637470
François Pluchino <francois.pluchino@klipper.dev>
2022-06-26 13:22:19 +0200

    Update design of confirm modals

--- components/KBtnConfirmation/KBtnConfirmation.vue
+++ components/KBtnConfirmation/KBtnConfirmation.vue
@@ -28,7 +28,7 @@ file that was distributed with this source code.
         >
             <v-card>
                 <v-card-title
-                    :class="$classes('primary--text', 'text--lighten-2')"
+                    :class="$classes('', 'text--lighten-2')"
                 >
                     <slot
                         name="title"
