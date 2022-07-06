b930c234885fcdc607d52fb2edf12f8a39ce347d
François Pluchino <francois.pluchino@klipper.dev>
2022-07-06 16:47:13 +0200

    Add KRequest component

---
+++ components/KRequest/KRequest.vue
@@ -0,0 +1,126 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts">
+import {AjaxContent} from '@klipper/bow/composables/mixins/http/ajaxContent';
+import {defineComponent, PropType} from '@vue/composition-api';
+import {CreateElement, Props, RenderContext, VNode} from 'vue';
+
+export interface RequestSlotProps {
+    loading: boolean;
+    error: HttpClientRequestError|null;
+    previousRequests: CancelerBag,
+    get errorCode(): number;
+    get errorMessage(): string;
+    finishLoading(): void;
+    fieldErrors(field: string): string[];
+    resetPreviousError(): void;
+}
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export default defineComponent({
+    name: 'KRequest',
+
+    mixins: [
+        AjaxContent,
+    ],
+
+    props: {
+        request: {
+            type: Function as PropType<<D>(canceler: Canceler, payload?: Dictionary<any>) => Promise<D|null>>,
+            required: true,
+        },
+
+        payload: {
+            type: Object as PropType<Dictionary<any>>,
+            default: undefined,
+        },
+
+        noErrorMessage: {
+            type: Boolean,
+            default: false,
+        },
+
+        hookAfterRequest: {
+            type: Function,
+            default: undefined,
+        },
+    },
+
+    computed: {
+        genSlotProps(): RequestSlotProps {
+            return {
+                loading: this.loading,
+                error: this.previousError,
+                previousRequests: this.previousRequests,
+                errorCode: this.errorCode,
+                errorMessage: this.errorMessage,
+                finishLoading: this.finishLoading,
+                fieldErrors: this.fieldErrors,
+                resetPreviousError: this.resetPreviousError,
+                fetch: this.fetch,
+            };
+        },
+    },
+
+    methods: {
+        async fetch<D>(): Promise<D|null> {
+            const res = await this.fetchData<D>(this.request, !this.noErrorMessage, true);
+
+            if (!this.previousError) {
+                this.$emit('success', res);
+            } else {
+                this.$emit('error', {
+                    error: this.previousError,
+                    previousRequests: this.previousRequests,
+                    errorCode: this.errorCode,
+                    errorMessage: this.errorMessage,
+                    fieldErrors: this.fieldErrors,
+                    resetPreviousError: this.resetPreviousError,
+                });
+            }
+
+            return res;
+        },
+
+        hookAfterFetchDataRequest(): void {
+            if (!!this.hookAfterRequest) {
+                this.hookAfterRequest();
+            } else {
+                this.finishLoading();
+            }
+        },
+
+        /**
+         * @override
+         */
+        async callRequest<D>(canceler: Canceler, request: (canceler: Canceler) => Promise<D|null>): Promise<D|null> {
+            return await request(canceler, this.payload);
+        },
+    },
+
+    render(createElement: CreateElement, context: RenderContext<Props>): VNode | VNode[] {
+        const defaultSlot = this.$scopedSlots.default ? this.$scopedSlots.default(this.genSlotProps) : undefined;
+
+        if (0 === defaultSlot.length) {
+            return defaultSlot;
+        }
+
+        return createElement('div', {
+            class: 'k-request',
+
+            props: this.$attrs,
+
+            on: this.$listeners,
+        }, defaultSlot);
+    },
+});
+</script>
