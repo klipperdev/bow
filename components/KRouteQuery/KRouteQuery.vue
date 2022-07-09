6ad93e8e21bbf0211a771a2f2c80e6644024c71a
François Pluchino <francois.pluchino@klipper.dev>
2022-07-09 13:13:54 +0200

    Add KRouteQuery component

---
+++ components/KRouteQuery/KRouteQuery.vue
@@ -0,0 +1,129 @@
+<!--
+This file is part of the Klipper package.
+
+(c) François Pluchino <francois.pluchino@klipper.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+-->
+
+<script lang="ts">
+import {defineComponent} from '@vue/composition-api';
+import {CreateElement, Props, RenderContext, VNode} from 'vue';
+import {replaceRouteQuery, restoreRouteQuery} from '@klipper/bow/utils/router';
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+export default defineComponent({
+    name: 'KRouteQuery',
+
+    props: {
+        query: {
+            type: String,
+            required: true,
+        },
+
+        queryPrefix: {
+            type: String,
+            default: undefined,
+        },
+
+        defaultValue: {
+            default: undefined,
+        },
+
+        valueType: {
+            type: String,
+            default: undefined,
+        },
+
+        /**
+         * Inject the query value in route if query is not defined in the route.
+         */
+        forceQuery: {
+            type: Boolean,
+            default: false,
+        }
+    },
+
+    data(): Dictionary<any> {
+        return {
+            isMounted: false as boolean,
+            queryValue: undefined as string|number,
+        };
+    },
+
+    computed: {
+        genSlotProps(): Dictionary<any> {
+            return {
+                attrs: this.genAttrsProps,
+                on: this.genOnProps,
+                queryValue: this.queryValue,
+            };
+        },
+
+        genAttrsProps(): Dictionary<any> {
+            return {
+                value: this.queryValue,
+            };
+        },
+
+        genOnProps(): Dictionary<any> {
+            return {
+                change: this.onChange,
+            };
+        },
+    },
+
+    mounted(): void {
+        const queryValue = restoreRouteQuery<any>(
+            this.query,
+            this.$route,
+            this.queryPrefix,
+            this.defaultValue,
+            this.valueType,
+        );
+
+        if (undefined !== queryValue) {
+            this.queryValue = queryValue;
+        }
+
+        this.isMounted = true;
+
+        if (this.forceQuery) {
+            this.onChange(this.queryValue);
+        }
+    },
+
+    methods: {
+        onChange(value: string|number): void {
+            this.queryValue = value;
+
+            if (this.isMounted) {
+                replaceRouteQuery(
+                    {
+                        [this.query]: this.queryValue,
+                    },
+                    this.$route,
+                    this.queryPrefix,
+                );
+            }
+        },
+    },
+
+    render(createElement: CreateElement, context: RenderContext<Props>): VNode | VNode[] {
+        const defaultSlot = this.$scopedSlots.default ? this.$scopedSlots.default(this.genSlotProps) : undefined;
+
+        if (0 === defaultSlot.length) {
+            return defaultSlot;
+        }
+
+        return createElement('div', {
+            class: 'k-route-query',
+            props: this.$attrs,
+            on: this.$listeners,
+        }, defaultSlot);
+    },
+});
+</script>
